328c4ece386a179926fe34e68c9a9a4ed1e599fb
Nursaera Ismy Pradandi <113576099+saeraismy@users.noreply.github.com>
2024-09-05 11:01:59 +0700

    Add files via upload

---
+++ tugas/koneksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+import com.mysql.cj.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    private static Connection koneksi; 
+    public static Connection GetConnection() throws SQLException{ 
+        if (koneksi == null){ 
+        new Driver(); 
+        koneksi = 
+        DriverManager.getConnection("jdbc:mysql://localhost:3306/db_dokter?zeroDateTimeBehavior=CONVERT_TO_NULL","root",""); 
+        } 
+        return koneksi; 
+    } 
+}
